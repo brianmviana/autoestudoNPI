df359a63c3984864cdcb061363ae79bfc3695f83
Brian M. Viana <brianmviana@gmail.com>
2018-11-11 17:30:49 -0300

    Configurando Spring com tocante externo no Maven Builder

--- src/main/java/br/ufc/npi/AutoestudoApplication.java
+++ src/main/java/br/ufc/npi/AutoestudoApplication.java
@@ -2,10 +2,16 @@ package br.ufc.npi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class AutoestudoApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AutoestudoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoestudoApplication.class, args);
