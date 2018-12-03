7e81fb7ccd924da7f4c9c7b3a1d605e4e01e393a
Brian M. Viana <brianmviana@gmail.com>
2018-12-03 03:27:19 -0300

    teste unitarios

--- src/test/java/br/ufc/npi/JogadorControllerTest.java
+++ src/test/java/br/ufc/npi/JogadorControllerTest.java
@@ -1,46 +1,82 @@
 package br.ufc.npi;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import br.ufc.npi.bean.Jogador;
 import br.ufc.npi.controller.JogadorController;
+import br.ufc.npi.service.JogadorService;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JogadorControllerTest {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+    private WebApplicationContext webApplicationContext;
+
 	@Autowired
 	private JogadorController controller;
 
-	private MockMvc mockMvc;
-	@Before
-	public void setUp() {
-		mockMvc = MockMvcBuilders.standaloneSetup(new JogadorController()).build();
-	}
+	@MockBean
+    private JogadorService jogadorServiceMock;
 
+    Jogador jogador;
+    @Before
+    public void setUpProduct() throws Exception{
+    	jogador = new Jogador();
+    	jogador.setId(1);
+    	jogador.setNome("TESTE");
+    	jogador.setIdade(21);
+    }
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+	
 
 	@Test
 	public void jogadorControler() throws Exception {
 		assertThat(controller).isNotNull();
 	}
 
-	@Test
-	public void testIndex() throws Exception{
-		this.mockMvc.perform(get("/jogadores/"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("jogadores"))
-		.andDo(print());
-	}
+    @Test
+    public void testList() throws Exception {
+        assertThat(this.jogadorServiceMock).isNotNull();
+        mockMvc.perform(MockMvcRequestBuilders.get("/jogadores/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("jogadores"))
+                .andExpect(MockMvcResultMatchers.view().name("jogadores"))
+                .andExpect(content().string(Matchers.containsString("Jogadores")))
+                .andDo(print());
+    }
+    
+//    @Test
+//    public void addTest() throws Exception {
+//    	assertThat(this.jogadorServiceMock).isNotNull();
+//    	mockMvc.perform(MockMvcRequestBuilders.post("/jogadores/salvar", jogador))
+//    	.andExpect(status().isOk())
+//        .andExpect(view().name("jogadores"));
+//    }
 }
